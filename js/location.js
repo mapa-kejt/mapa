81c133727f14b655d51539571dcbe074f234fa6f
mapa-kejt <135213494+mapa-kejt@users.noreply.github.com>
2023-06-01 14:21:19 +0200

    Update location.js

--- js/location.js
+++ js/location.js
@@ -1,22 +1,75 @@
-/*POSITION*/
+document.addEventListener('DOMContentLoaded', function() {
+  handleLocationPermission();
+});
 
-navigator.geolocation.getCurrentPosition(function (position) {
+function handleLocationPermission() {
+  if (navigator.permissions) {
+    navigator.permissions.query({ name: 'geolocation' }).then(function(result) {
+      if (result.state === 'granted') {
+        getCurrentLocation();
+      } else if (result.state === 'prompt') {
+        requestLocationPermission();
+      }
+    });
+  } else {
+    getCurrentLocation();
+  }
+}
+
+function getCurrentLocation() {
+  navigator.geolocation.getCurrentPosition(function(position) {
     var userLatitude = position.coords.latitude;
     var userLongitude = position.coords.longitude;
-  
-    // Map user's coordinates to SVG map
-    var svgMap = document.getElementById('svg-map');
-    var svgPoint = svgMap.createSVGPoint();
-    svgPoint.x = userLongitude; // Map longitude to SVG x-coordinate
-    svgPoint.y = userLatitude; // Map latitude to SVG y-coordinate
-    var mappedPoint = svgPoint.matrixTransform(svgMap.getScreenCTM());
-  
-    // Add a marker to the SVG map
-    var marker = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    marker.setAttribute('cx', mappedPoint.x);
-    marker.setAttribute('cy', mappedPoint.y);
-    marker.setAttribute('r', 5);
-    marker.setAttribute('fill', 'red');
-    svgMap.appendChild(marker);
+    displayUserLocation(userLatitude, userLongitude);
   });
-   
+}
+
+function requestLocationPermission() {
+  navigator.geolocation.getCurrentPosition(
+    function(position) {
+      getCurrentLocation();
+    },
+    function(error) {
+      console.error('Error getting location:', error);
+    }
+  );
+}
+
+function displayUserLocation(latitude, longitude) {
+  const mapWidth = 1817; // Width of the SVG map in pixels
+  const mapHeight = 984; // Height of the SVG map in pixels
+  
+  var svgMapParent = document.getElementById('zoom');
+  var svgMapRect = svgMapParent.getBoundingClientRect();
+
+  // Calculate the cx and cy values for each point
+  var points = [
+    { id: 'svetaStosija', latitude: 44.127204894681576, longitude: 15.23749511295614 }, // Add coordinates for point 1
+    { id: 'svetiRoko', latitude: 44.12636170880619, longitude: 15.238860383372325 }, // Add coordinates for point 2
+    
+    // Add more points as needed
+  ];
+
+  points.forEach(function(point) {
+    var svgPoint = document.getElementById(point.id);
+    var cx = (point.longitude + 180) * (mapWidth / 360);
+    var cy = ((90 - point.latitude) * (mapHeight / 180));
+
+    // Display the blue marker based on the user's proximity to each point
+    var distance = calculateDistance(latitude, longitude, point.latitude, point.longitude);
+    if (distance <= 0.05) { // Distance in kilometers (50 meters = 0.05 kilometers)
+      var marker = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      marker.setAttribute('cx', cx);
+      marker.setAttribute('cy', cy);
+      marker.setAttribute('r', 12);
+      marker.setAttribute('fill', 'blue');
+      svgPoint.parentNode.appendChild(marker);
+    }
+  });
+}
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  // Implement the Haversine formula or any other distance calculation formula here
+  // ...
+  return distance; // Distance in kilometers
+}
